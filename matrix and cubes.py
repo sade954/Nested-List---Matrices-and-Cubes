6272ed617394f99845e4693da256abd3fd4d665a
Candace Hollinger <csig1985@gmail.com>
2022-12-12 01:22:24 +0000

    update matrix and cubes

---
+++ matrix and cubes.py
@@ -0,0 +1,11 @@
+#Creating a matrix for a list
+my_matrix = [[1, 2, 3], 
+             [4, 5, 6]]
+my_matrix
+[[1, 2, 3], [4, 5, 6]]
+row count = len(my_matrix)
+row count
+2
+column_count = len(my_matrix[0])
+column count
+3
